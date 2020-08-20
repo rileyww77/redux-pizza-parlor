4aa16b6466cd1e4b8f7e497bba74ce2cef086df0
riley_ww77 <riley_ww77@yahoo.com>
2020-08-20 13:08:42 -0500

    basic setup done. installed necessary apps. set up base index page

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { logger } from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+const store = createStore(
+    combineReducers({
+    
+    }),   
+    applyMiddleware(logger),
+ );
+  
+  
+ ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
